767dd9acf7ff9fe6788baf3dbaf4d3717600d38b
elliefoote <86239914+elliefoote@users.noreply.github.com>
2025-08-11 13:50:54 +1000

    Simplify (#8)
    * icons
    
    * change pkg
    
    * rm unnecessary
    
    * update readme
    
    * try again
    
    * projects section
    
    * small updates
    
    * fix
    
    * rm p-lock.json
    
    * fix (kind of) type error
    
    * wip
    
    * update Readme
    
    * projects
    
    * simplify
    
    ---------
    
    Co-authored-by: Ellie Foote <ellie@Ellies-MacBook-Air.local>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,7 @@
-import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
-
 import siteMetadata from '@/data/siteMetadata'
 
-import { sortedBlogPost, allCoreContent } from '@/lib/utils/contentlayer'
-import { InferGetStaticPropsType } from 'next'
-import { allBlogs } from 'contentlayer/generated'
-import BlogCard from '@/components/BlogCard'
-import { RoughNotation } from 'react-rough-notation'
-
-const MAX_DISPLAY = 3
-
-export const getStaticProps = async () => {
-  // TODO: move computation to get only the essential frontmatter to contentlayer.config
-  const sortedPosts = sortedBlogPost(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-
-  return { props: { posts } }
-}
-
-export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home() {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
@@ -29,49 +11,10 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
             Hi, I'm Ellie 👋
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            I'm a software engineer, currently living in sunny Sydney, Australia. I created this
-            space as a way to document my journey in tech, as well as a place for me to share{' '}
-            <RoughNotation
-              type="underline"
-              show={true}
-              color="#06b6d4"
-              animationDelay={800}
-              animationDuration={1200}
-            >
-              projects{' '}
-            </RoughNotation>
-            and
-            <RoughNotation
-              type="underline"
-              show={true}
-              color="#eab308"
-              animationDelay={800}
-              animationDuration={1200}
-            >
-              {' '}
-              reflections
-            </RoughNotation>
-            .
+            I'm a software engineer, currently living in sunny Sydney, Australia.
           </p>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            return <BlogCard post={post} key={post.slug + post.date} />
-          })}
-        </ul>
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="all posts"
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-      )}
     </>
   )
 }
